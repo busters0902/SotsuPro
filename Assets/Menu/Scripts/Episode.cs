c04e27f69223162ab630b9edffbab6ada63d0bea
busters0902 <busters0902@hotmail.co.jp>
2017-10-26 16:07:56 +0900

    サンプルの追加

---
+++ Assets/Menu/Scripts/Episode.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Episode : MonoBehaviour
+{
+
+	[SerializeField, Tooltip("子のテキストを設定")]
+	Text text;
+
+	[SerializeField, Tooltip("自身のトランスフォーム")]
+	RectTransform rectTrans;
+	public RectTransform RectTrans { get { return rectTrans; } }
+
+	[SerializeField, Tooltip("自身のボタン")]
+	Button button;
+
+    //この話のキャラのID
+    public int CharID { get; private set; }
+
+    //この話の章のID
+    public int ChapID { get; private set; }
+
+    //この話のID
+    public int EpiID { get; private set; }
+
+    bool isHave;
+	public bool IsHave
+	{
+		get { return isHave; }
+		set { isHave = value; }
+	}
+
+	void Start()
+	{
+
+        //Button button = this.GetComponent<Button>();
+        //button.onClick.AddListener(() => { Debug.Log("Clicked." + text.text); });
+        button.onClick.AddListener(() => {
+            Debug.Log("OnClick " + "キャ;ラ " + CharID + ", " + ChapID + "章, " + EpiID + "話");
+            DataManager.Instance.nowReadCharcterID = CharID;
+
+            DataManager.Instance.nowReadRecordStoryID = EpiID;
+            DataManager.Instance.nowReadRecordChapterID = ChapID;
+            Fade.Instance.FadeIn(0.5f, () => { SceneChanger.LoadScene("ADV", true); });
+        });
+        //string str = "キャラ " + CharID +", " + ChapID + "章, "+ EpiID + "話";
+
+
+        //var num = text.text.Length;
+        //※仕様変更
+        //文字列が長い場合　ボタンを大きくする
+        //if(num > 3)
+        //{
+        //	var min = RectTrans.offsetMin;
+        //	var max = RectTrans.offsetMax;
+        //	//RectTrans.offsetMax =  new Vector2( num * 50f + 50f  ,1f);
+        //	RectTrans.offsetMin = new Vector2(-(num * 50f + 50f)/2, min.y);
+        //	RectTrans.offsetMax = new Vector2((num * 50f + 50f)/2,  max.y);
+        //}
+
+        //ボタンを大きめに作る
+        var min = RectTrans.offsetMin;
+		var max = RectTrans.offsetMax;
+		var num = 8f;
+		RectTrans.offsetMin = new Vector2(-(50f)*num / (float)2, min.y);
+		RectTrans.offsetMax = new Vector2( (50f)*num / (float)2, max.y);
+
+
+	}
+
+	public void Setup(string name = "N")
+	{
+		//Button button = this.GetComponent<Button>();
+		button.onClick.AddListener(() => { Debug.Log("Click : " + name + " : " + text.text  ); });
+	}
+
+	public void SetName( string name)
+	{
+		text.text = name;
+	}
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="data">初期化を行うためのデータ</param>
+	public void Setup( EpisodeData data )
+	{
+
+        //どの話がわかるデータ類
+        CharID = data.charID;
+        ChapID = data.chapID;
+        EpiID = data.epiID;
+
+        //データマネージャーの攻略情報から
+        //この話を取得しているかを判別する
+        var finData = DataManager.Instance.finishedStoryData[CharID];
+        Debug.Log("char" + CharID + "finData" + finData.finishedReadChapterID+ ":"+ finData.finishedReadStoryID);
+        if (ChapID < finData.finishedReadChapterID)
+        {
+            isHave = true;
+        }
+        else if (ChapID == finData.finishedReadChapterID && EpiID <= finData.finishedReadStoryID)
+        {
+            isHave = true;
+        }
+        else if(finData.finishedReadChapterID == 13)
+        {
+            isHave = true;
+        }
+        else
+        {
+            isHave = false;
+        }
+
+        //isHave = data.isHave;
+
+        if (isHave)
+		{
+			text.text = data.name;
+		}
+		else
+		{
+			text.text = "？？？";
+			button.interactable = false;
+		}
+	}
+
+
+}
---
+++ Assets/Menu/Scripts/ResetManager.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetManager : MonoBehaviour
+{
+
+	[SerializeField, Tooltip("確認ウィンドウ")]
+	GameObject window1;
+
+	[SerializeField, Tooltip("再確認ウィンドウ")]
+	GameObject window2;
+
+	[SerializeField, Tooltip("リセットを終えたウィンドウ")]
+	GameObject window3;
+
+	[SerializeField, Tooltip("キャラ選択ウィンドウ")]
+	GameObject window4;
+
+	[SerializeField, Tooltip("キャラ選択確認ウィンドウ")]
+	GameObject window5;
+
+	void Start()
+	{
+
+		Reset();
+
+	}
+
+	//ゲームオブジェクトのリセット
+	public void Reset()
+	{
+		//各ウィンドウの初期状態
+		window1.SetActive(false);
+		window2.SetActive(false);
+		window3.SetActive(false);
+		window4.SetActive(true);
+		window5.SetActive(false);
+
+		characterIDBuffer = -1;
+	}
+
+
+	public void OnClickWindow1Yes()
+	{
+		Debug.Log("ウィンドウ①　Yes");
+
+		//二つ目のウィンドウへ
+		window1.SetActive(false);
+		window2.SetActive(true);
+
+	}
+
+	public void OnClickWindow1No()
+	{
+		Debug.Log("ウィンドウ①　No");
+
+		//コンテンツ画面に戻る？
+		MenuManager.Instance.ShiftBack();
+	}
+
+	public void OnClickWindow2Yes()
+	{
+		Debug.Log("ウィンドウ②　Yes");
+
+		//削除
+		SaveData.ResetMasteringData();
+		SaveData.ResetFinishedStoryData();
+		DataManager.Instance.masteringData = new DataManager.MasteringData();
+		for (int i = 0; i < 4; i++)
+		{
+			DataManager.Instance.finishedStoryData[i] = new DataManager.FinishedStoryData();
+		}
+		DataManager.Instance.nowReadStoryID = -1;
+		DataManager.Instance.nowReadChapterID = -1;
+
+		//三つ目の画面へ
+		window2.SetActive(false);
+		window3.SetActive(true);
+	}
+
+	public void OnClickWindow2No()
+	{
+		Debug.Log("ウィンドウ②　No");
+
+		//一つ目の画面へ
+		window2.SetActive(false);
+		window1.SetActive(true);
+
+
+	}
+
+	//変更したいキャラのIDを一時的に覚えておく
+	int characterIDBuffer;
+
+	//キャラクター選択
+	public void OnClickCharacter(int CharacterID_)
+	{
+		characterIDBuffer = CharacterID_;
+
+		//キャラ変更確認画面へ
+		window4.SetActive(false);
+		window5.SetActive(true);
+
+	}
+
+	//キャラ変更する
+	public void OnClickChangeCharacterYes()
+	{
+		Debug.Log("ウィンドウ⑤　Yes");
+
+		if (Fade.Instance.isFade == true) return;
+
+		DataManager.Instance.masteringData.masteringCharacterID = characterIDBuffer;
+		//ConvertADVData.Instance.SetMasteringCharacterLastStoryID();
+		//DataManager.Instance.masteringData.masteringCharacterLastStoryID = 1;
+		//DataManager.Instance.masteringData.masteringCharacterLastChapterID = 0;
+		DataManager.Instance.nowReadStoryID = 0;
+		DataManager.Instance.nowReadChapterID = 0;
+		DataManager.Instance.endLine = 1;
+        DataManager.Instance.masteringData.itemNum = 0;
+		DataManager.Instance.masteringData.likeabillity = 0;
+
+		Debug.Log("キャラクターデータの変更完了");
+
+		SaveData.SaveMasteringData();
+
+		Fade.Instance.FadeIn(0.5f, () => { SceneChanger.LoadScene("MyPage"); });
+	}
+
+	//キャラ変更しない
+	public void OnClickChangeCharacterNo()
+	{
+		Debug.Log("ウィンドウ⑤　No");
+
+		//コンテンツ画面に戻る？
+		MenuManager.Instance.ShiftBack();
+	}
+
+
+}
