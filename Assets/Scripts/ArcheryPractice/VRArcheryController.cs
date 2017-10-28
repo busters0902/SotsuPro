b34d02b571bf0d8bf010950b21a5874bd7e2a971
2F-DT-02\vantan <busters0902@hotmail.co.jp>
2017-10-28 18:32:30 +0900

    練習ができる

---
+++ Assets/Scripts/ArcheryPractice/VRArcheryController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRArcheryController : MonoBehaviour
+{
+    [SerializeField]
+    Bow bow;
+
+    [SerializeField]
+    SteamVR_TrackedObject rController;
+
+    [SerializeField]
+    SteamVR_TrackedObject lController;
+
+    [SerializeField]
+    TrackingTransform arrowCamera;
+
+    private void Start()
+    {
+        //ViveController.Instance.SetLeftController(rController);
+        //ViveController.Instance.SetRightController(lController);
+    }
+
+    void Update()
+    {
+        var lDevice = ViveController.Instance.GetLeftDevice();
+        var rDevice = ViveController.Instance.GetRightDevice();
+
+
+        if ( lDevice.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            bow.CreateArrow();
+        }
+        else if (lDevice.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            bow.UpdateDraw(0.01f);
+        }
+        else if (lDevice.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            bow.Shoot();
+            arrowCamera.target = bow.arrow.transform;
+        }
+
+    }
+}
