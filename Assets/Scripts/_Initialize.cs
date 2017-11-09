d3afba9375661b291508404bf945d43220e7ffcb
busters0902 <busters0902@hotmail.co.jp>
2017-11-09 14:10:41 +0900

    初期化クラスの追加

---
+++ Assets/Scripts/_Initialize.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _Initialize : MonoBehaviour
+{
+
+	/// <summary>
+	/// ゲーム起動時にシーンが呼ばれる前に一度だけ呼ばれるメソッド
+	/// Managerの生成
+	/// </summary>
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+	static void Initialize()
+	{
+		//データマネージャーの生成
+		Debug.Log("_Initialize");
+		//GameObject obj = new GameObject("Manager", typeof(DataManager));
+		//GameObject.DontDestroyOnLoad(obj);
+
+		//フェードマネージャーの作成
+		//var fadeObj = (GameObject)Instantiate(Resources.Load("Prefabs/FadeManager"));//(GameObject)Resources.Load("Prefabs/FadeManager");
+		//DontDestroyOnLoad(fadeObj);
+
+		//サウンドマネージャーの作成
+		var soundObj = (GameObject)Instantiate(Resources.Load("Prefabs/SoundManager"));
+		DontDestroyOnLoad(soundObj);
+
+
+		//スクリーンの向き
+		//Screen.orientation = ScreenOrientation.LandscapeLeft;
+		//var asset = Resources.Load<InitializeAsset>("InitializeAsset");
+
+		//Application.targetFrameRate = asset.targetFps;
+		Debug.Log("target fps: " + Application.targetFrameRate);
+	}
+
+}
