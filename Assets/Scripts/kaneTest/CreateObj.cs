6c5a9d7601026a7236b26aa68be4a5f0bf8c1287
peperontino39 <peperrontino39@docomo.ne.jp>
2017-11-13 13:57:39 +0900

    とりあえずプッシュ

---
+++ Assets/Scripts/kaneTest/CreateObj.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObj : MonoBehaviour {
+
+    [SerializeField]
+    GameObject pre;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var obj = Instantiate(pre);
+            obj.GetComponent<Arrow3>().Shot(CalculatedData.Create(Vector3.forward,1));
+            obj.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+	}
+}
