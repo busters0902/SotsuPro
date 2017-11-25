b5ac353c6d9b0db95a8805d64c70a1e7aa230c77
peperontino39 <peperrontino39@docomo.ne.jp>
2017-11-25 17:39:57 +0900

    ブレ軽減の処理のテストを作成

---
+++ Assets/Scripts/kaneTest/burekeigen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class burekeigen : MonoBehaviour {
+    Quaternion prevQuat;
+	// Use this for initialization
+	void Start () {
+        prevQuat = transform.parent.rotation;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+         var angle = 
+        Quaternion.Angle(transform.rotation, prevQuat);
+
+        if(angle > 1)
+        {
+            transform.rotation = Quaternion.identity; 
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, prevQuat, 0.9f);
+        }
+            Debug.Log(angle);
+
+
+        prevQuat = transform.parent.rotation;
+    }
+}
