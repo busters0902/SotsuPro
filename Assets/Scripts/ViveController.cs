86ae0b7f721ca07da700d8b1c61541403eba0680
busters0902 <busters0902@hotmail.co.jp>
2017-11-27 06:19:54 +0900

    こまかい修正

--- Assets/Scripts/ViveController.cs
+++ Assets/Scripts/ViveController.cs
@@ -47,17 +47,30 @@ public class ViveController : MonoBehaviour
         Debug.Log("左コントローラーの接続");
 	}
 
+    //コルーチン内では通常のデバイスからの判定を行えないので
     bool viveRightDown;
+    public bool ViveRightDown{ get { return viveRightDown; } }
     bool viveRightUp;
+    public bool ViveRightUp { get { return viveRightUp; } }
     bool viveRight;
+    public bool ViveRight { get { return viveRight; } }
     bool viveLeftDown;
+    public bool ViveLeftDown { get { return viveLeftDown; } }
     bool viveLeftUp;
+    public bool ViveLeftUp { get { return viveLeftUp; } }
     bool viveLeft;
+    public bool ViveLeft { get { return viveLeft; } }
 
     Vector2 rightAxis;
+    public Vector2 RightAxis { get { return rightAxis; } }
+
+    Vector2 leftAxis;
+    public Vector2 LeftAxis { get { return leftAxis; } }
 
     private void Update()
     {
+
+        //右
         var device = ViveController.Instance.GetRightDevice();
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
             viveRightDown = true;
@@ -78,6 +91,7 @@ public class ViveController : MonoBehaviour
         else
             viveRight = false;
 
+        //左
         var device2 = ViveController.Instance.GetLeftDevice();
         if (device2.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
             viveLeftDown = true;
@@ -93,11 +107,16 @@ public class ViveController : MonoBehaviour
         else
             viveLeft = false;
 
-        float valueX = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).x;
-        float valueY = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).y;
-        rightAxis = new Vector2(valueX, valueY);
+        float valueRX = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).x;
+        float valueRY = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).y;
+        rightAxis = new Vector2(valueRX, valueRY);
 
         //Debug.Log("VRコン: " + rightAxis);
+
+        float valueLX = device2.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).x;
+        float valueLY = device2.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).y;
+        rightAxis = new Vector2(valueLX, valueLY);
+
     }
 
 }
