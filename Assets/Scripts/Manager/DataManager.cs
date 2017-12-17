b6e6b1bc4f13cd0c56ee13314767d3b01cc50010
busters0902 <busters0902@hotmail.co.jp>
2017-12-18 03:24:35 +0900

    スコア、データマネージャーの追加

---
+++ Assets/Scripts/Manager/DataManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    static private DataManager instance = null;
+    static public DataManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                var obj = new GameObject("DataManager");
+                var ins = obj.AddComponent<DataManager>();
+                instance = ins;
+            }
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        //Debug.Log(this + " Awake");
+        if (instance != null)
+        {
+            //Debug.Log("Destroy " + this);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    static private ScoreManager instance = null;
+    static public ScoreManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                var obj = new GameObject("ScoreManager");
+                var ins = obj.AddComponent<ScoreManager>();
+                instance = ins;
+                //Debug.Log(obj + " NullInstance");
+            }
+            return instance;
+        }
+    }
+    
+    void Awake()
+    {
+        //Debug.Log(this + " Awake");
+        if(instance != null)
+        {
+            //Debug.Log("Destroy " + this);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
