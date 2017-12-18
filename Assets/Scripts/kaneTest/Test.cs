585b5506b20590cb03778fddecfa252ab57cc34e
peperontino39 <peperrontino39@docomo.ne.jp>
2017-12-18 12:00:12 +0900

    風のParticle

--- Assets/Scripts/kaneTest/Test.cs
+++ Assets/Scripts/kaneTest/Test.cs
@@ -6,11 +6,14 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        FadeControl.Instance.FadeStart();
+        //FadeControl.Instance.FadeStart();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetMouseButtonDown(0))
+        {
+            AudioManager.Instance.PlaySE("引き絞り");
+        }
 	}
 }
